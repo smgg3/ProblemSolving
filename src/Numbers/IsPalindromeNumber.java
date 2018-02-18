5ea7f978ec1ddfccedbf5a80904edbef6e3defcc
Sravani Murakonda <Sravani.Murakonda@Cerner.com>
2018-02-17 19:24:16 -0600

    Adding isPalindromeNumber program and its junits

---
+++ src/Numbers/IsPalindromeNumber.java
@@ -0,0 +1,36 @@
+package Numbers;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IsPalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x != 0) {
+            int l = x / div;
+            int r = x % 10;
+            if (l != r)
+                return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        IsPalindromeNumber obj = new IsPalindromeNumber();
+        assertTrue(obj.isPalindrome(101));
+        assertTrue(obj.isPalindrome(0));
+        assertFalse(obj.isPalindrome(-1));
+        assertFalse(obj.isPalindrome(129));
+    }
+
+}
